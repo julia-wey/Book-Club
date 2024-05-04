90db363bfa5b608c48275c6f5a8ce1e8410f2284
Julia Wey-Burgoyne <juliawey@gmail.com>
2024-05-04 12:25:03 -0400

    ADD: Book Container and Book Card styling.

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,12 +1,21 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function BookCard({title, image}) {
     return (
-        <li className="card">
-            <img src={image} alt={title} />
-            <h4>{title}</h4>
-        </li>
-    );
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={image} />
+              <Card.Body>
+                {/* <Card.Title>Card Title</Card.Title> */}
+                {/* <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text> */}
+                <Button variant="primary">Details</Button>
+              </Card.Body>
+            </Card>
+          );
 }
 
 export default BookCard;
