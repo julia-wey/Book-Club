5f1b8adbb0f18faf44cab7c49dc6dc7b21331ada
Julia Wey-Burgoyne <juliawey@gmail.com>
2024-05-08 22:26:27 -0400

    Adds home styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,8 @@ import React from "react";
 function Home() {
     
     return (
-        <div >
-            <h1>“Books are a uniquely portable magic.” —Stephen King</h1>
-            <h3>Welcome to Book Club!</h3>
-            <h4>We meet once a month to discuss what we've read. Click "Our Books" to see what we're reading.</h4>
+        <div className="home">
+            <h2 className="quote">“Books are a uniquely portable magic.” —Stephen King</h2>
         </div>
     )
 }
